1637390b6f51dd5cbda2377abddad6b78a3b0769
wolede <wolede.adeniyi@gmail.com>
2020-01-24 23:42:08 +0100

    added hunta red variables, button paddings and border radius

---
+++ src/pages/home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import SEO from "../components/seo"
+import { Button } from 'reactstrap';
+
+const Home = () => {
+    return (
+        <div>
+            <SEO title="Home" />
+            <Button color="hunta-red">Sup Bish</Button>
+        </div>
+    )
+}
+
+export default Home
