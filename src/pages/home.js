6ce489d9dd9d7310b0bf2bca86719fb0fe59f61f
wolede <wolede.adeniyi@gmail.com>
2020-01-25 00:24:59 +0100

    setting font sizes

--- src/pages/home.js
+++ src/pages/home.js
@@ -6,6 +6,7 @@ const Home = () => {
     return (
         <div>
             <SEO title="Home" />
+            <h1 className="hero-text">Discover</h1>
             <Button color="hunta-red">Sup Bish</Button>
         </div>
     )
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,9 @@
 import React from "react"
-// import { Link } from "gatsby"
-import Layout from "../components/layout"
-// import 'bootstrap/dist/css/bootstrap.css';
 import Home from "./home";
 import '../styles/global.scss';
 
 const IndexPage = () => (
-  <Layout>
-    {/* <Link to="/page-2/">Go to page 2</Link> */}
-    <Home />
-  </Layout>
+    <Home/>
 )
 
 export default IndexPage
