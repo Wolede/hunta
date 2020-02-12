b708d3a0c01f96f83e02787ab5342b6641e3c2ce
wolede <wolede.adeniyi@gmail.com>
2020-02-12 04:50:57 +0100

    couldn't fix iframe load scroll

--- src/components/embed/embed.js
+++ src/components/embed/embed.js
@@ -2,27 +2,33 @@ import React, { useState } from 'react'
 
 const Embed = () => {
     const [loading, setLoading] = useState(true);
+    // const [iframeFocus, setIframeFocus] = useState(false);
 
     const hideLoader = () => {
-        setLoading(!loading)        
+        setLoading(!loading)  
+        // setTimeout(() => {
+        //         setIframeFocus(!iframeFocus)
+        // }, 2000);
     }
 
     return (
         <div>
             {loading ? (
                 <div className="loading-placeholder">
-
+                    {/* loader here  */}
                 </div>
             ) : null}
-
-            <iframe id='travelstartIframe-6de3ec3b-c31c-49b6-b9fa-76ef1cede4b9'
-            frameBorder='0'
-            scrolling='yes'
-            src="https://www.travelstart.com/search-on-index?search=false&show_banners=true&log=true&affId=198718&language=&affCampaign=&utm_source=affiliate&utm_medium=198718&isiframe=true&landing_page=false&iframeVersion=11&host=http://www.huntagroup.com/destinations"
-            style={{ width : '100%', minHeight : '23rem' }}
-            onLoad={hideLoader}
-            >
-            </iframe>
+            {/* <div className={iframeFocus ? ' ' : 'invisible'}> */}
+            <div style={ loading ? { display : 'none' } : { display : 'block' }}>
+                <iframe id='travelstartIframe-6de3ec3b-c31c-49b6-b9fa-76ef1cede4b9'
+                frameBorder='0'
+                scrolling='yes'
+                src="https://www.travelstart.com/search-on-index?search=false&show_banners=true&log=true&affId=198718&language=&affCampaign=&utm_source=affiliate&utm_medium=198718&isiframe=true&landing_page=false&iframeVersion=11&host=http://www.huntagroup.com/destinations"
+                style={{ width : '100%', minHeight : '23rem' }}
+                onLoad={hideLoader}
+                >
+                </iframe>
+            </div>
         </div>
     )
 }
