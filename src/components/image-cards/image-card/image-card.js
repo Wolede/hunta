14a272b65dbb08a3259a04c48b8251cf077a6cfb
wolede <wolede.adeniyi@gmail.com>
2020-02-18 22:16:56 +0100

    looped through tours and link them to their appropriate dynamic routes

--- src/components/image-cards/image-card/image-card.js
+++ src/components/image-cards/image-card/image-card.js
@@ -1,17 +1,21 @@
 import React from 'react'
 import BackgroundImage from '../../background-image/background-image'
 import { Card, CardTitle } from 'reactstrap';
+import { Link } from 'gatsby';
 // import PoolSide from '../../../images/PoolSide.jpg'
-const ImageCard
- = () => {
+const ImageCard = (props) => {
+    console.log(props, 'nested');
+    
     return (
         <div className="image-card">
-            <Card>
-                {/* <CardImg top width="100%" src={PoolSide} alt="Card image cap" className="image-card-image"/> */}
-                <BackgroundImage src="PoolSide.jpg" htmlTag="div" htmlClass="image-card-image" bgColor="#000000"></BackgroundImage>
-                <CardTitle className="text-red">SOUTH AFRICA</CardTitle>
-                <h5 className="h5">A Family Trip to Cape Town</h5>
-            </Card>
+            <Link to={`/tours/${props.tour.id}`}>
+                <Card>
+                    {/* <CardImg top width="100%" src={PoolSide} alt="Card image cap" className="image-card-image"/> */}
+                    <BackgroundImage src="PoolSide.jpg" htmlTag="div" htmlClass="image-card-image" bgColor="#000000"></BackgroundImage>
+                    <CardTitle className="text-red">SOUTH AFRICA</CardTitle>
+                    <h5 className="h5">A Family Trip to Cape Town</h5>
+                </Card>
+            </Link>
         </div>
     )
 }
--- src/components/image-cards/image-cards.js
+++ src/components/image-cards/image-cards.js
@@ -1,12 +1,48 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 import { Row, Col } from 'reactstrap';
 import ImageCard from './image-card/image-card';
 
 const ImageCards = () => {
+    const [loading, setLoading] = useState(true);
+    const [tours, setTours] = useState();
+
+    const baseUrl = 'http://hunta-api.herokuapp.com/tours'
+
+    const hideLoader = () => {
+        setLoading(!loading)
+    }
+
+    useEffect(() => {
+        axios.get(`${baseUrl}`).then(res => {
+            console.log(res);
+            setTours(res.data);
+            console.log(tours, "tours");        
+            hideLoader();
+        })
+        .catch(error => {
+            console.log(error);
+        })
+    }, [])
+
     return (
         <Row>
             <Col sm="4" md="3">
-                <ImageCard/>
+                {
+                    loading ? (
+                    <div className="loading-placeholder">
+                        {/* loader here  */}
+                    </div>
+                ) 
+                :
+                    tours.map((tour, index) => (
+                        <ImageCard
+                        key={index}
+                        tour={tour}
+                        />
+                    ))
+                }
+                
             </Col>
         </Row>
     )
